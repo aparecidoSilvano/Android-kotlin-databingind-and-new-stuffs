3048983e48e653ca5c0f91a106bf91a10b2cff04
José Aparecido Silvano de Albuquerque <silvanoalbuquerque@gmail.com>
2018-11-21 05:41:37 -0300

    Adding a simple project that uses the new android concept for navigation between views.

---
+++ Jetpack-navigation-example/app/src/main/java/com/example/aparecido/androidnavigationexample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.aparecido.androidnavigationexample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val host = NavHostFragment.create(R.navigation.nav_graph)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, host)
+            .setPrimaryNavigationFragment(host).commit()
+    }
+}
